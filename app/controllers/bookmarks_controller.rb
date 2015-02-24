e012db15135e94479540cce4db0dc1ac04f5d5df
luismenjivar <luismenjivr@gmail.com>
2015-02-24 17:14:55 -0500

    added create_bookmark_from_mail to skip_before_action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create_bookmark]
+  skip_before_action :verify_authenticity_token, only: [:create_bookmark_from_mail]
 
   def create_bookmark_from_mail
     @user = User.where(email: "#{params[:sender]}").first
@@ -14,9 +14,10 @@ class BookmarksController < ApplicationController
       else
         Bookmark.create(url: "#{@url}", topic_id: @topic.id)
       end
-        head 200
-      else
-        head 202
-    end  
+      head 200
+    else
+      head 202
+    end 
+
   end
 end
