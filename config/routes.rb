cdc05a257102e5cbd38283ab115a0a27603df297
luismenjivar <luismenjivr@gmail.com>
2015-02-24 16:35:52 -0500

    changed actions name in routes:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#index"
-  post :bookmarks, to: 'bookmarks#create_bookmark'
+  post :bookmarks, to: 'bookmarks#create_bookmark_from_mail'
 end
