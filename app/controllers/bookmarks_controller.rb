849c15063f1b89ef72b790185b0ce6c6e5c41ec6
luismenjivar <luismenjivr@gmail.com>
2015-02-24 16:00:16 -0500

    configured #create_bookmark_from_mail, changed routes file, deleted incoming controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,22 @@
 class BookmarksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create_bookmark]
 
-
- 
-
+  def create_bookmark_from_mail
+    @user = User.where(email: "#{params[:sender]}").first
+    @topic = Topic.where(title: "#{params[:subject]}", user_id: @user.id).first
+    @url = params["body-plain"]
+    
+    if @user != nil 
+      
+      if @topic.nil?
+        @new_topic = Topic.create(title: "#{params[:subject]}", user_id: @user.id)
+        Bookmark.create(url: "#{@url}", topic_id: @new_topic.id)
+      else
+        Bookmark.create(url: "#{@url}", topic_id: @topic.id)
+      end
+        head 200
+      else
+        head 202
+    end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#index"
-  post :incoming, to: 'bookmarks#create_bookmark'
+  post :bookmarks, to: 'bookmarks#create_bookmark'
 end
