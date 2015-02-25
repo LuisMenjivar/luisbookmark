17046454d816189b4e898ec39f14850a1a0dca2d
luismenjivar <luismenjivr@gmail.com>
2015-02-25 15:32:19 -0500

    added two examples to request spec

--- spec/requests/create_bookmark_from_email_spec.rb
+++ spec/requests/create_bookmark_from_email_spec.rb
@@ -13,9 +13,16 @@ describe "Bookmarks request " do
     expect(@user.topics.first.bookmarks).to match_array [Bookmark.find_by(url: "www.google.com")]
     expect(response.status).to eq(200) 
   end
-  xit "creates a bookmark if topic already exists" do
+  it "creates a bookmark if topic already exists" do
+    topic = create(:topic, title: "animals", user: @user)
+    post '/bookmarks',{sender: "test@example.com", subject: "animals", "body-plain": "www.google.com"}
+    expect(@user.topics.first.bookmarks.first).to belong_to(:topic)
   end
-  xit "does not create a topic nor a bookmark if user does not exists" do 
+  it "does not create a topic nor a bookmark if user does not exists", :skip_before do
+    post '/bookmarks',{sender: "test@example.com", subject: "animals", "body-plain": "www.google.com"}
+    expect(Topic.all.count).to eq(0)
+    expect(Bookmark.all.count).to eq(0)
+    expect(response.status).to eq(200) 
   end
   xit "does not create a bookmark without a topic" do 
   end
