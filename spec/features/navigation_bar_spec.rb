1000cff1b900bcbb6859d7ecedab9e6e04b7fc53
luismenjivar <luismenjivr@gmail.com>
2015-02-25 18:55:55 -0500

    modified nav bar, and added corresponding specs

--- spec/features/navigation_bar_spec.rb
+++ spec/features/navigation_bar_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 feature "Navigation bar" do 
-  scenario "has link sign in and home link when user is not signed on" do 
+  before(:example) do |example|
+    unless example.metadata[:skip_before]
+      @user = create(:user)
+      manual_sign_in(@user)
+      visit root_path
+    end
+  end
+
+  scenario "has sign in link, sign up link, and home link when user is not signed in", :skip_before do 
     visit root_path
     expect(page.body).to have_link("Home")
     expect(page.body).to have_link("Sign in") 
+    expect(page.body).to have_link("Sign up")
   end
-  scenario "has account and sign out link when user is signed in " do 
-    visit root_path
-    user = create(:user)
-    manual_sign_in(user)
+  scenario "has topics, account, and sign out link when user is signed in " do 
     expect(page.body).to have_link("Home")
     expect(page.body).to have_link("Sign out") 
     expect(page.body).to have_link("Account")
-    #body 
+    expect(page.body).to have_link("Topics")
   end
 end
