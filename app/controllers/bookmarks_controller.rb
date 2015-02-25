c12aa8a0386d9fc6a73619bff6779da55519aa41
luismenjivar <luismenjivr@gmail.com>
2015-02-25 16:57:09 -0500

    Removed unnecessary string interpolation

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,16 +2,16 @@ class BookmarksController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create_bookmark_from_mail]
 
   def create_bookmark_from_mail
-    user = User.where(email: "#{params[:sender]}").first
+    user = User.where(email: params[:sender]).first
   
     if user != nil 
-       topic = Topic.where(title: "#{params[:subject]}", user_id: user.id).first
+       topic = Topic.where(title: params[:subject], user: user).first
        url = params["body-plain"]
       if topic.nil?
-        new_topic = Topic.create!(title: "#{params[:subject]}", user: user)
-        Bookmark.create!(url: "#{url}", topic: new_topic)
+        new_topic = Topic.create!(title: params[:subject], user: user)
+        Bookmark.create!(url: url, topic: new_topic)
       else
-        Bookmark.create(url: "#{url}", topic: topic)
+        Bookmark.create(url: url, topic: topic)
       end
       head 200
     else
