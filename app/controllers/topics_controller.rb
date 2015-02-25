6f6def22a1d7006b0eb3c5b9d4400cc0d12dba0e
luismenjivar <luismenjivr@gmail.com>
2015-02-25 10:56:34 -0500

    added topic show action and view. Modified Topic controller. Modified user_views_topic_index_spec

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,9 +5,18 @@ class TopicsController < ApplicationController
     @topics = current_user.topics
   end
   
+  def show
+    @topic_bookmarks = @topic.bookmarks
+  end
+
   private
-  
+
   def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  
+  def topic_params
     params.require(:topic).permit(:title, :user_id)
   end
 end
--- spec/features/user_views_topic_index_spec.rb
+++ spec/features/user_views_topic_index_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
+feature "Topics" do 
 
-feature "user views topic index" do 
-  scenario "only topics that belong to user" do 
-    user = create(:user)
+ before(:each) do 
+    @user = create(:user)
+    manual_sign_in(@user)
+ end
+
+  scenario "User can browse through an index of his topics" do 
     topic = create(:topic, user_id: nil)
-    manual_sign_in(user)
     visit topics_path
     expect(page).to have_css("h1", text: "Topics") 
     expect(page).not_to have_css("h4", text: "#{topic.title}") 
-    topic.update(user_id: user.id)
+    topic.update(user_id: @user.id)
     visit topics_path
     expect(page).to have_css("h4", text: "#{topic.title}") 
   end
+
+  scenario "user can browse through an index of his bookmarks" do 
+    topic = create(:topic, user_id: @user.reload.id)
+    bookmark = create(:bookmark, topic_id: topic.id)
+    visit topics_path
+    click_link("#{topic.title}")
+    expect(page).to have_css("a", text: "#{bookmark.url}")
+  end
 end
+
+
+
