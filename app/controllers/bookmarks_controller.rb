20b3d7e336ab7534c0935470a825522467cd4b23
luismenjivar <luismenjivr@gmail.com>
2015-02-25 14:16:44 -0500

    added an example and a before block to request spec. Modified Bookmarks#create_bookmark_from_mail to set topic and url variables only if the user is not nil

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,21 +2,20 @@ class BookmarksController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create_bookmark_from_mail]
 
   def create_bookmark_from_mail
-    @user = User.where(email: "#{params[:sender]}").first
-    @topic = Topic.where(title: "#{params[:subject]}", user_id: @user.id).first
-    @url = params["body-plain"]
-    
-    if @user != nil 
-      
-      if @topic.nil?
-        @new_topic = Topic.create(title: "#{params[:subject]}", user_id: @user.id)
-        Bookmark.create(url: "#{@url}", topic_id: @new_topic.id)
+    user = User.where(email: "#{params[:sender]}").first
+  
+    if user != nil 
+       topic = Topic.where(title: "#{params[:subject]}", user_id: user.id).first
+       url = params["body-plain"]
+      if topic.nil?
+        new_topic = Topic.create!(title: "#{params[:subject]}", user: user)
+        Bookmark.create(url: "#{url}", topic: new_topic)
       else
-        Bookmark.create(url: "#{@url}", topic_id: @topic.id)
+        Bookmark.create(url: "#{url}", topic: topic)
       end
       head 200
     else
-      head 202
+      head 200
     end 
 
   end
--- spec/requests/create_bookmark_from_email_spec.rb
+++ spec/requests/create_bookmark_from_email_spec.rb
@@ -1,10 +1,24 @@
 require "rails_helper"
 describe "Bookmarks request " do 
-  it "creates a bookmark " do
-    user = create(:user, email: "test@example.com")
+
+  before(:example) do |example|
+    unless example.metadata[:skip_before]
+      @user = create(:user, email: "test@example.com")
+    end
+  end
+
+  it "creates a topic and a bookmark " do
     post '/bookmarks',{sender: "test@example.com", subject: "animals", "body-plain": "www.google.com"}
-    expect(user.reload.topics).to match_array [Topic.find_by(title: "animals")]
-    expect(user.topics.first.bookmarks).to match_array [Bookmark.find_by(url: "www.google.com")]
-    expect(response.status).to eq(200)
+    expect(@user.reload.topics).to match_array [Topic.find_by(title: "animals")]
+    expect(@user.topics.first.bookmarks).to match_array [Bookmark.find_by(url: "www.google.com")]
+    expect(response.status).to eq(200) 
+  end
+  xit "creates a bookmark if topic already exists" do
+  end
+  xit "does not create a topic nor a bookmark if user does not exists" do 
+  end
+  xit "does not create a bookmark without a topic" do 
+  end
+  xit "does not create an empty bookmark" do 
   end
 end
