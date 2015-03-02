e7fce5ca888c75fc5c0cc720c044fb9414bfa6dd
luismenjivar <luismenjivr@gmail.com>
2015-03-02 18:10:05 -0500

    modified LikesController create action to buidl and save a like. destroy action to find and destroy a like. And to raise notice or error if like was created or not

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,27 @@
 class LikesController < ApplicationController
   def create
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.build(bookmark: @bookmark)
+    
+    if like.save
+      flash[:notice] = "Bookmark was liked successfuly"
+      redirect_to [@bookmark.topic]
+    else
+      flash[:error] = "Error liking this bookmark"
+      redirect_to [@bookmark.topic]
+    end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.find(params[:id])
+    
+    if like.destroy
+      flash[:notice] = "Bookmark is not liked anymore"
+      redirect_to [@bookmark.topic]
+    else
+      flash[:error] = "Error unliking this bookmark"
+      redirect_to [@bookmark.topic]
+    end
   end
 end
