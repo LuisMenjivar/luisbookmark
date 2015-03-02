58fdf0a290bfd2f4ad8f8a98428b5ea85203cabb
luismenjivar <luismenjivr@gmail.com>
2015-03-02 16:33:54 -0500

    nested likes create and destroy in bookmarks resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#index"
   resources :topics do 
-    resources :bookmarks
+    resources :bookmarks do 
       resources :likes, only: [:create, :destroy]
+    end
   end
   post :bookmarks, to: 'bookmarks#create_bookmark_from_mail'
 end
