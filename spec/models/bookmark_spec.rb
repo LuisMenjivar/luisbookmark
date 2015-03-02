83a22104bd536ad1afb2f3c2762ac9c056c7d918
luismenjivar <luismenjivr@gmail.com>
2015-03-02 16:03:16 -0500

    added User/Topic/Like/Bookmark model specs

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do 
+    it "belongs to topic" do 
+      expect(Bookmark.new).to belong_to(:topic)
+    end
+    it "has many likes" do 
+      expect(Bookmark.new).to have_many(:likes)
+    end
+  end
 end
