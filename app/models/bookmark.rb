94ac6a56d1750b208e22006cab2f785ddb16b306
luismenjivar <luismenjivr@gmail.com>
2015-03-05 17:51:26 -0500

    Installed paperclip gem. Created migration to add image to bookmark. Configured paperclip in Bookmark model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,6 @@ class Bookmark < ActiveRecord::Base
   validates :url, presence: true
   belongs_to :topic
   has_many :likes, dependent: :destroy
+  has_attached_file :image, :styles => { :thumb => "100x100>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
